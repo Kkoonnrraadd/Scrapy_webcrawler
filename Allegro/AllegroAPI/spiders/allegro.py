52cbc69c219de024f30c4ba06836f8e51dddd270
WarniWarni <47402988+WarniWarni@users.noreply.github.com>
2019-11-26 16:27:54 +0100

    nic szczegolnego

--- Allegro/AllegroAPI/spiders/allegro.py
+++ Allegro/AllegroAPI/spiders/allegro.py
@@ -11,7 +11,7 @@ from scrapy.loader import ItemLoader
 
 class ElectronicsSpider(scrapy.Spider):
     name = 'allegro_spider'
-    allowed_domains = ["www.allegro.com.pl"]
+    allowed_domains = ["www.allegro.com.pl", "www.allegro.pl"]
     def start_requests(self): # jest uniwersalny dla kazdej podstrony dla kategorii Elektronika
         #tutaj trzeba doprecyowac przez ktore podkategorie ma przejsc pajak
         #multitasking przy zapytaniu o kilka produktow
@@ -19,10 +19,10 @@ class ElectronicsSpider(scrapy.Spider):
         urls=['https://allegro.pl/kategoria/konsole-i-automaty?bmatch=baseline-cl-dict42-ele-1-1-1024']    #telefony i akcesoria
 
         for url in urls:
-            yield Request(url,callback=self.parse)
+            yield Request(url, callback=self.parse)
 
-    def parse(self,response):
-
+    def parse(self, response):
+        # loader = ItemLoader()
         for item in self.scrape(response):  #iterator
               yield item
 
@@ -30,27 +30,19 @@ class ElectronicsSpider(scrapy.Spider):
         print("Found url: {}".format(next))
         yield Request(response.urljoin(next), callback=self.parse, dont_filter=True)     #POST requests byly przekierowywane, dont filter ratuje sutyacje
 
-    def scrape(self,response):   # trzeba przekazac do itemu
+    def scrape(self, response):   # trzeba przekazac do itemu
 
         #brakuje automatycznego wchodzenia w link artykulu i pobiwrania oceny
-        paths= response.css('section._5a7713f')
-        articles=paths.css('article')
+        paths = response.css('section._5a7713f')
+        articles = paths.css('article')
         for article in articles:
-            #article_url = article.css('h2 a::attr(href)').get()              #link do artykulu
-            #jeden=response.css('div._9a071_1LGgN')
-            #dwa=jeden.css('div::text').getall()
-            #trzy=dwa[2]                                           #OPINIA ale po wejsciu na ULR produktu
+            article_url = article.css('h2 a::attr(href)').get()              #link do artykulu
+            jeden=response.css('div._9a071_1LGgN')
+            dwa=jeden.css('div::text').getall()
+            trzy=dwa[2]                                           #OPINIA ale po wejsciu na ULR produktu
 
             yield {  #produkty ze strony glownej
                 'title': article.css('h2 a::text').get(),
                 'price': article.css("div._906bb92 span::text").get(),
                 'decimal' : article.css("div._906bb92 span:nth-child(2)::text").get()
             }
-
-
-
-
-
-
-
-
