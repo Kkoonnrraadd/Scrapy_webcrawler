4c6ee17a67a56366238fa8a7029e74f0f76fa7ee
WarniWarni <47402988+WarniWarni@users.noreply.github.com>
2019-12-03 21:26:06 +0100

    Podstawowe zapytania

---
+++ AllegroApi/data_extractor.py
@@ -0,0 +1,17 @@
+import json
+
+def extract_seller_id(json_file):
+    pass
+
+def extract_delivery_price(json_file):
+    pass
+
+def extract_price(json_file):
+    pass
+
+def extract_availability(json_file):
+    pass
+
+def extract_name_and_id(json_file):
+    pass
+
--- AllegroApi/main.py
+++ AllegroApi/main.py
@@ -3,14 +3,31 @@ import json
 
 
 RestApi = AllegroRestApi()
-
 # RestApi.load_token()
 
-status_code, json_data = RestApi.resource_get(
-        resource_name='/offers/listing',
-        params={'phrase': 'Dell Inspiron 7347 i5-4210U/8GB/256/Win8 FHD Dotyk',
-                'limit' : 1}
-        )
 
-with open('zapytanie.json', 'w') as zapytanie:
-    json.dump(json_data, zapytanie)
+def get_response(phrase, limit=10, sorting='+price'):
+    status_code, json_data = RestApi.resource_get(
+        resource_name='/offers/listing',
+        params={'phrase': phrase,
+                'limit': limit,
+                'sort': sorting}
+    )
+    return status_code, json_data
+
+
+def save_json(json_data, filename='zapytanie.json'):
+    with open(filename, 'w') as zapytanie:
+        json.dump(json_data, zapytanie)
+
+
+def get_extracted_data(phrase):
+    status_code, json_data = get_response(phrase, limit=40)
+    print(type(json_data))
+    for typ_oferty in json_data['items']:
+        print(typ_oferty)
+        for oferty in json_data['items'][typ_oferty]:
+            print(oferty)
+    # save_json(json_data['items'])
+
+get_extracted_data('wibrator analny')
