0bcd34c1c938ae8a450402abedf962920b6adee9
WarniWarni <47402988+WarniWarni@users.noreply.github.com>
2019-12-03 20:47:00 +0100

    Pozyskanie tokena

---
+++ AllegroApi/get_token.py
@@ -0,0 +1,15 @@
+from pyAllegro.api import AllegroRestApi, AllegroWebApi
+from AllegroApi import config
+
+RestApi = AllegroRestApi()
+#RestApi = AllegroRestApi(config_file_dir='/Users/xszpo/.allegroApiConfig')
+
+RestApi.credentials_set(
+        appName=config.app_name,
+        clientId=config.client_id,
+        clientSecred=config.client_secret,
+        redirectUrl='http://localhost:8000'
+        )
+
+RestApi.get_token()
+
