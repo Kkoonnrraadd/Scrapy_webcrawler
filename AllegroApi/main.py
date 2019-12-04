8fa2630e4fafb0933732f654c4e7c30be65a1c10
WarniWarni <47402988+WarniWarni@users.noreply.github.com>
2019-12-04 23:44:16 +0100

    hierarchizacja danych

--- AllegroApi/main.py
+++ AllegroApi/main.py
@@ -17,7 +17,6 @@ RestApi = AllegroRestApi()
 # GUI/cokolwiek
 
 
-
 def get_extracted_data(phrase, limit=20, sorting='+price', minumum_price=0, maximum_price=999999999):
     status_code, json_data = fetch_module.get_response(RestApi, phrase, limit, sorting, minumum_price, maximum_price)
     # save_json(json_data, filename='odpowiedz_surowa.json')
@@ -34,22 +33,50 @@ def get_ex_seller_data(phase, sellerId): #funkcja ktora ma na celu sprawdzenie c
     return item_list_to_compare  # ?
 
 
-def write_responses_to_a_list(list_of_items_to_search):
+def write_responses_to_a_list(list_of_items_to_search): #dict
     responses_list = []
+    # responses_dict = {}
     for item in list_of_items_to_search:  # dla kazdej frazy zwroc wyekstrachowana liste itemow na allegro
+        # responses_dict[item] = get_extracted_data(item)
         responses_list.append(get_extracted_data(item))  # wez ten szajs i dodaj go do listy?
         # moze to nie bedzie zbyt szybkie, ale proste do napisania...
     return responses_list
+    # return responses_dict
 
 
 def run_this_program():
-    products_count = user_interaction.insert_count()
-    input_table = user_interaction.input_user(products_count)
-    first_order_responses = write_responses_to_a_list(input_table)  # lista bezposrednich odpowiedzi na nasze zapytanie
-    print(first_order_responses)
+    # products_count = user_interaction.insert_count()
+    # input_table = user_interaction.input_user(products_count)
+    input_table = ['Kasza', 'Kurczaki']  # temporary
+    first_order_responses = write_responses_to_a_list(input_table)  # DICT lista bezposrednich odpowiedzi na nasze zapytanie
+    # print(type(first_order_responses))
+    utils.save_json(first_order_responses)
+    sellers = get_sellers(first_order_responses)
+    utils.save_json(sellers, 'sellers.json')
+    # dostajemy listę bezpośrednich odpowiedzi na nasze zapytanie.
+    #
+    # Teraz trzeba uzyskać listę sprzedawców dla każdego szukanego artykułu
+    #
+    #
     # seller_table = checkSeller.getSellers()  # tej tablicy
     # checkSeller.show()
 
+
+def get_sellers(first_order_responses):
+    sellers = {}
+    print('first order response type : {}'.format(type(first_order_responses)))
+
+    for item in first_order_responses:
+        print(item)
+        print('item type: {}'.format(type(item)))
+        # print(first_order_responses[item])
+        inner_dict = {}
+        for offer in item:
+            print('offer type: {}'.format(type(offer)))
+            inner_dict[offer['seller']['id']] = offer['offer_id']
+        sellers[item] = inner_dict
+    return sellers
+# run_this_program()
 
 # for i in input_table:
 #     get_ex_seller_data(i,'49703356') # tutaj jak wprowadzisz z palca seller.id to smiga, ale jak juz przekazuje z tej tablicy
